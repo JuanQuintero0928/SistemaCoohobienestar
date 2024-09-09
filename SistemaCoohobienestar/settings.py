11609520024be0a9f1d3233d66bdc6f86bde7d38
Juan Jose <108360551+JuanQuintero0928@users.noreply.github.com>
2024-09-09 13:03:04 -0500

    archivos static

--- SistemaCoohobienestar/settings.py
+++ SistemaCoohobienestar/settings.py
@@ -130,10 +130,11 @@ LOGOUT_REDIRECT_URL = reverse_lazy('dashboard')
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 
-STATICFILES_DIRS = [
-    BASE_DIR /  "static/"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR /  "static/"
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
