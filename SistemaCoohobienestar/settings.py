73cdbdd798a078925a9288fd5136e57379c4fb50
Juan Jose <jjquintero0928@gmail.com>
2025-12-25 11:55:26 -0500

    ajuste en template modalpago

--- SistemaCoohobienestar/settings.py
+++ SistemaCoohobienestar/settings.py
@@ -27,9 +27,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', default=False)
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 # Application definition
 
